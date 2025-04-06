c9754c6c38c75dd22f992cdbbe16164de20254ec
sufyanmoon9090 <sufyanmoon9090@gmail.com>
2025-04-07 00:02:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '263780166288';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '263780166288' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '923276254377' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ᴊɪɴᴡᴏᴏ ᴠ4';
-global.pairingNumber = process.env.BOT_NUMBER || '263780166288' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'i4wnHboS#KjHlXXXQay70ERcIKZWOlUT8-_BhzcKkCXI_rgrDpW4' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '923276254377' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'j4QQDb4B#vjU4in8D1jRKAR_5dIOMSCyX6CEJ_V-dM6kavp9_u_g' ;  // put your session id here
 
 global.mods = []
 global.prems = []
